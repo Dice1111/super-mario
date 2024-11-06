aa2e13772089ecf4c009d0c7ff1fc0f54b722a7f
ClaraXinn <claraxin8@gmail.com>
2024-11-06 20:03:35 +0800

    landing_page
    front-end

--- app/(Home)/layout.tsx
+++ app/(Home)/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import Header from "@/components/Header/Header";
 import { ReactNode } from "react";
+import Landing from "@/components/Landing/Landing";
 
 export const metadata: Metadata = {
   title: "Super Mario",
@@ -14,7 +15,10 @@ export default async function RootLayout({ children }: RootLayoutProps) {
   return (
     <div>
       <Header />
-      <main className="relative container top-20 mx-auto px-2 sm:px-0">
+
+      <Landing />
+
+      <main className="relative container mx-auto px-2 sm:px-0 pt-20">
         {children}
       </main>
     </div>
---
+++ components/Search/BuyerSearch/BuyerSearchBar.tsx
@@ -0,0 +1,45 @@
+"use client";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+interface SearchBarProps {
+  onSearch?: (query: string) => void;
+}
+
+export default function BuyerSearchBar({ onSearch }: SearchBarProps) {
+  const [query, setQuery] = useState("");
+
+  const handleSearch = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (onSearch) {
+      onSearch(query);
+    }
+  };
+
+  return (
+    <div className="flex items-center justify-center ">
+      <div className="text-center">
+        <h1 className="font-bold text-2xl mb-4">Explore.</h1>
+        <form
+          onSubmit={handleSearch}
+          className=" flex items-center justify-center p-4"
+        >
+          <Input
+            type="text"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            placeholder="Search for car"
+            className="px-4 py-2 w-full max-w-xs border rounded-md"
+          />
+          <Button
+            type="submit"
+            className="px-4 py-2 ml-2 bg-red-500 text-white rounded-md"
+          >
+            Search
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
+}
