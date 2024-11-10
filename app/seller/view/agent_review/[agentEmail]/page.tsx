5fa3218a01e73791a1faf76ff12524d20ad61ada
mario <khantkokozaw382000@gmail.com>
2024-11-10 16:59:50 +0800

    add review page for agent

---
+++ app/seller/view/agent_review/[agentEmail]/page.tsx
@@ -0,0 +1,28 @@
+"use client";
+import CreateReviewAndRatingUI from "@/app/boundaries/BuyerUI/CreateReviewAndRatingUI";
+import ViewRatingAndReviewUI from "@/app/boundaries/UserUI/ViewRatingAndReviewUI";
+import { use } from "react";
+
+interface Props {
+  params: Promise<{ agentEmail: string }>;
+}
+
+const UsedCarDetailPage = ({ params }: Props) => {
+  const unwrappedParams = use(params);
+  const decodedEmail = decodeURIComponent(unwrappedParams.agentEmail);
+  console.log(decodedEmail);
+  const createReviewBoundary = CreateReviewAndRatingUI.getInstance();
+  const viewReviewBoundary = ViewRatingAndReviewUI.getInstance();
+
+  return (
+    <>
+      <h1 className="text-2xl font-bold">Review of {decodedEmail}</h1>
+      <br />
+      <br />
+      {createReviewBoundary.displayCreateReviewAndRatingUI(decodedEmail)}
+      {viewReviewBoundary.displayRatingAndReviewUI(decodedEmail)}
+    </>
+  );
+};
+
+export default UsedCarDetailPage;
